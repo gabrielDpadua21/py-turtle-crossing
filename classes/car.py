7f8921d9e5b832b4f51e5d9a87bfcffc0dd05c62
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-07-05 23:34:33 -0300

    refactor: ♻️ car class create and move cars

--- classes/car.py
+++ classes/car.py
@@ -1,9 +1,28 @@
 from turtle import Turtle
+from random import choice, randint
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 START_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-class Car(Turtle):
-    pass
+class Car:
+
+    def __init__(self) -> None:
+        self.all_cars = []
+
+
+    def create(self) -> None:
+        chance = randint(1, 6)
+        if chance == 1:
+            car = Turtle("square")
+            car.shapesize(stretch_wid=1, stretch_len=2)
+            car.penup()
+            car.color(choice(COLORS))
+            random_y = randint(-250, 250)
+            car.goto(300, random_y)
+            self.all_cars.append(car)
+        
+    def move(self) -> None:
+        for car in self.all_cars:
+            car.backward(START_MOVE_DISTANCE)
         
--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from turtle import Screen
 from time import sleep
 from classes.avatar import Avatar
+from classes.car import Car
 
 
 if __name__ == "__main__":
     screen = Screen()
     screen.setup(width=600, height=600)
     screen.title("Turtle Game")
+    screen.tracer(0)
     screen.listen()
     GAME_ON = True
 
     avatar = Avatar()
+    cars = Car()
 
     screen.onkey(avatar.move_up, "Up")
 
     while GAME_ON:
         sleep(0.1)
         screen.update()
+        cars.create()
+        cars.move()
 
 
     screen.exitonclick()
