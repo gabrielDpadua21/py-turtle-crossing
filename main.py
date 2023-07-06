6d01af49bbcd4e116618a6581dfb0ed9ed4e2296
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-07-05 23:04:27 -0300

    feat: ✨ setup screen game

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from turtle import Screen
+from time import sleep
+from classes.avatar import Avatar
 
 
 if __name__ == "__main__":
-    print("Template python")
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.title("Turtle Game")
+    screen.listen()
+    GAME_ON = True
+
+    avatar = Avatar()
+
+    screen.onkey(avatar.move_up, "Up")
+    screen.onkey(avatar.move_down, "Down")
+
+    while GAME_ON:
+        sleep(0.1)
+        screen.update()
+
+
+    screen.exitonclick()
     
